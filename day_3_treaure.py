925f6448463cd12201e5522ba98adef5da44500c
jlotto8 <jlotto8@gmail.com>
2023-10-03 14:49:28 -0400

    ok

---
+++ day_3_treaure.py
@@ -0,0 +1,46 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+l_or_r = input('Do you want to go left or right? Please type left or right  ')
+l_or_r = l_or_r.lower()
+if l_or_r == 'right':
+  print('You stepped in hot lava! Game Over.')
+elif l_or_r == 'left':
+  swim_ques = input('Do you want to swim, or wait? Please enter swim or wait. ')
+  if swim_ques == 'swim':
+    print('Oh no! Crocodiles have infested these waters! Game Over.')
+  elif swim_ques == 'wait':
+    doors = input("Three doors appear. Do you want to enter the red door, the yellow door, or the blue door? ")
+    if doors == "red":
+      print("You entered a burning hot building and turned to ash. Game Over.")
+    elif doors == "blue":
+      print("You entered a hanuted jungle and a spooky gorilla ghost ate you. Game Over.")
+    elif doors == "yellow":
+      print('Congratulations! You won the game!')
+else: 
+      print("Game Over.")
